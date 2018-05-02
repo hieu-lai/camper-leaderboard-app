1967e578523aa00ded712004efe7846c0cdc71a6
Hieu Lai <hieuqlai@gmail.com>
2018-05-02 14:43:54 +1000

    setup webpack

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CamperLeaderboardApp from './components/CamperLeaderboardApp';
+
+ReactDOM.render(<CamperLeaderboardApp />, document.getElementById('app'));
---
+++ src/components/CamperLeaderboardApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+console.log('work');
+export default class CamperLeaderboardApp extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Camper Leaderboard</h1>
+      </div>
+    )
+  }
+}
